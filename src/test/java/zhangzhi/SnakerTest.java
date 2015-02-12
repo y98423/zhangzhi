9a7be1ddaee7d7cc975276e282083a48cfbcb35a
y98423 <775909767@qq.com>
2015-02-12 16:45:50 +0800

    Update SnakerTest.java

--- src/test/java/zhangzhi/SnakerTest.java
+++ src/test/java/zhangzhi/SnakerTest.java
@@ -6,7 +6,7 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class SnakerTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) { 
 		ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(
 				"spring/spring-x.xml");
 		SpringSnakerEngine engine = (SpringSnakerEngine) ctx.getBean("engine");
